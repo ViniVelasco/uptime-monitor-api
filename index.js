4dfa0c797b66bd96c1d964458a635bdf9645c219
= <sasukegc@gmail.com>
2018-12-15 19:47:46 -0200

    Roteamento para requisições criado

--- index.js
+++ index.js
@@ -30,11 +30,38 @@ var server = http.createServer(function(req,res) {
   req.on('end', function() {
     buffer += decoder.end()
 
-    // Send The Response
-    res.end('Hello World\n')
+    // Choose the handler this request should go to.
+    // If one is not found, send the not found handler
+    var choosenHandler = typeof(router[trinmedPath]) !== 'undefined' ? router[trinmedPath] : handlers.notFound
 
-    // Log the request path
-    console.log('Request received with this payload:', buffer)
+    // Construct the data object to the send to the handler
+    var data = {
+      'trinmedPath' : trinmedPath,
+      'queryStringObject' : queryStringObject,
+      'method' : method,
+      'headers' : headers,
+      'payload' : buffer
+    }
+
+    // Route the request to the handler specified in the router
+    choosenHandler(data, function(statusCode, payload) {
+      // Use the status code called back by the handler or default to 200
+      statusCode = typeof(statusCode) == 'number' ? statusCode : 200
+      
+      // Use the payload called back by the handler, or default to an empty object
+      payload = typeof(payload) === 'object' ? payload : {}
+
+      // Conver the payload to a string
+      var payloadString = JSON.stringify(payload)
+
+      // Return the response
+      res.writeHead(statusCode)
+      res.end(payloadString)
+
+      // Log the request path
+      console.log('Returning this response:', statusCode, payloadString)
+    })
+
   })
 
 })
@@ -43,3 +70,22 @@ var server = http.createServer(function(req,res) {
 server.listen(3000, function() {
   console.log("The server is listening on port 3000")
 })
+
+// Define the handlers
+var handlers = {}
+
+// Sample Handler
+handlers.sample = function(data,callback){
+  // Callback a http status code, and a payload object
+  callback(406, {'name' : 'sample handler'})
+}
+
+//Not found handler
+handlers.notFound = function(data, callback) {
+  callback(404)
+}
+
+// Define a request router
+var router = {
+  'sample': handlers.sample
+}
